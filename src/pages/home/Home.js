34d01ab0cc6bac1eddf9caed8b3397570aadfce4
Yubraj <imyubraz@gmail.com>
2023-05-18 19:01:15 +0545

    Icon used using import instead from public folder...

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Home.css"
 import Input from '../../components/input/Input'
 import Tasks from './../../components/tasks/Tasks';
+import icon from "./assets/images/icon.png"
 
 const Home = () => {
     return (
@@ -10,7 +11,8 @@ const Home = () => {
                 <div className="box">
                     <div className="row title">
                         <h1>To-Do List</h1>
-                        <img src="/icon.png" className='icon' alt="icon" />
+                        {/* <img src="/icon.png" className='icon' alt="icon" /> */}
+                        <img src={icon} className='icon' alt="icon" />
                     </div>
                     <div>
                         <Input />
